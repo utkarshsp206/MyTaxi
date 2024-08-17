1ee69c410261e4c1d28563e0bf52a18c771b806d
Utkarsh-Singh <utkarshsp206@gmail.com>
2024-08-17 14:41:54 +0530

    Confirmation Page Updated

--- app/payment-confirm/page.js
+++ app/payment-confirm/page.js
@@ -10,14 +10,14 @@ function PaymentConfirmation() {
     <div className='bg-[#f1f1f1] flex
     h-screen
     items-center justify-center flex-col'>
-        <h2 className='text-[30px] z-20 mt-[-30px]'>Payment Confimed</h2>
+        <h2 className='text-[30px] z-20 mt-[-30px]'>Booking Confimed</h2>
         <Image src='/uberconfirm.gif'
         width={500}
         height={150}
         className='object-cover mt-[-60px]'
         />
         <h2 className='font-bold 
-        text-[23px] mt-[-20px] mb-10'>Uber is Booked Successfully</h2>
+        text-[23px] mt-[-20px] mb-10'>Seat is Booked Successfully</h2>
         <button className='p-2 bg-black text-white
         px-10 rounded-lg'
         onClick={()=>route.push('/')}
