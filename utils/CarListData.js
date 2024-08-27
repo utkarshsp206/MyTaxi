9c43aaa300bcbd14778737958d175cf994175e90
Utkarsh-Singh <utkarshsp206@gmail.com>
2024-08-27 12:39:59 +0530

    Update carList Data

--- utils/CarListData.js
+++ utils/CarListData.js
@@ -10,36 +10,10 @@ export const CarListData = [
     {
         id: 2,
         name: 'Comfort',
-        seat: 4,
+        seat: 7,
         desc: 'Newer cars with extra legroom',
         amount: 1.6,
         image: '/UberComfort.png'
     },
-    {
-        id: 3,
-        name: 'ComfiXL',
-        seat: 6,
-        desc: 'Affordable rides for groups up to 6',
-        amount: 1.9,
-        image: '/UberXL_New.png'
-    },
-    {
-        id: 4,
-        name: 'Pet Friendly',
-        seat: 4,
-        desc: 'Affordable rides for you and your pet',
-        amount: 1.4,
-        image: '/UberX_Pet.png'
-    },
-    {
-        id: 5,
-        name: 'Premium Sedan',
-        seat: 4,
-        desc: 'Affordable, Everyday rides',
-        amount: 2,
-        image: '/Black_v1.png'
-    },
-
-
 
 ] 
