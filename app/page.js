653380776f7973222b3ed25e13f3b0f7132e14bb
Utkarsh-Singh <utkarshsp206@gmail.com>
2024-08-27 12:36:39 +0530

    app page.js preFinal

--- app/page.js
+++ app/page.js
@@ -1,39 +1,49 @@
-"use client"
+"use client";
 
-import GoogleMapSection from './../components/Home/GoogleMapSection'
-import SearchSection from './../components/Home/SearchSection'
-import { DestinationContext } from './../context/DestinationContext'
-import { SourceContext } from './../context/SourceContext'
-import { UserButton } from '@clerk/nextjs'
-import { LoadScript, useJsApiLoader } from '@react-google-maps/api'
-import Image from 'next/image'
-import { useState } from 'react'
-import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
+import GoogleMapSection from './../components/Home/GoogleMapSection';
+import SearchSection from './../components/Home/SearchSection';
+import { DestinationContext } from './../context/DestinationContext';
+import { SourceContext } from './../context/SourceContext';
+import { UserButton } from '@clerk/nextjs';
+import { useJsApiLoader } from '@react-google-maps/api';
+import Image from 'next/image';
+import { useState } from 'react';
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import Head from 'next/head';
 
 export default function Home() {
-  const [source,setSource]=useState([])
-  const [destination,setDestination]=useState([])
-  // const { isLoaded } = useJsApiLoader({
-  //   id: 'google-map-script',
-  //   googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY
-  // })
+  const [source, setSource] = useState([]);
+  const [destination, setDestination] = useState([]);
+
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+    libraries: ['places'],
+  });
+
+  if (!isLoaded) {
+    return <div>Loading...</div>; // Show loading message until the API is loaded
+  }
+
   return (
-    <SourceContext.Provider value={{source,setSource}}>
-      <DestinationContext.Provider value={{destination,setDestination}}>
-      <LoadScript 
-      libraries={['places']}
-      googleMapsApiKey={""}>
-      <div className='p-6 grid grid-cols-1 md:grid-cols-3 gap-5'>
-        <div>
-            <SearchSection/>
-            
-        </div>
-        <div className='col-span-2'>
-            <GoogleMapSection/>
+    <>
+      <Head>
+        <title>MY TAXI APP</title>
+        <meta name="description" content="Get Your bookings at your convenience." />
+      </Head>
+      <SourceContext.Provider value={{ source, setSource }}>
+        <DestinationContext.Provider value={{ destination, setDestination }}>
+          <div className='p-6 grid grid-cols-1 md:grid-cols-3 gap-5'>
+            <div>
+              <SearchSection />
+            </div>
+            {/* Hide the map on small devices and only show on large screens */}
+            <div className='hidden lg:block col-span-2'>
+              <GoogleMapSection />
+            </div>
           </div>
-      </div>
-      </LoadScript>
-      </DestinationContext.Provider>
-    </SourceContext.Provider>
-  )
+        </DestinationContext.Provider>
+      </SourceContext.Provider>
+    </>
+  );
 }
